2c9a4dc9991a78e02a06ac943e3f86b729f59701
Om-khairnar <om.khairnar175@gmail.com>
2024-02-22 00:36:21 +0530

    Forth commit

--- src/pages/components/SocialMediaIcons.jsx
+++ src/pages/components/SocialMediaIcons.jsx
@@ -1,55 +1,78 @@
-
+import Image from "next/image";
 const SocialMediaIcons = () => {
+  return (
+    <div className="flex justify-center md:justify-start my-10 gap-7">
+      <a
+        className="hover:opacity-50 transition-duration-500"
+        href="https://www.linkedin.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          alt="linkedin-link"
+          src="/assets/linkedin.png"
+          width={40}
+          height={40}
+        />
+      </a>
 
-    return (
-        <div className="flex justify-center md:justify-start my-10 gap-7">
-           <a
-            className="hover:opacity-50 transition-duration-500"
-            href="https://www.linkedin.com"
-            target="_blank"
-            rel="noreferrer"
-           >
-           <img alt="linkedin-link" src="/assets/linkedin.png"/>
-           </a>
+      <a
+        className="hover:opacity-50 transition-duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          alt="twitter-link" 
+          src="/assets/twitter.png"
+          width={40}
+          height={40}
+        />
+      </a>
 
-           <a
-            className="hover:opacity-50 transition-duration-500"
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noreferrer"
-           >
-           <img alt="twitter-link" src="/assets/twitter.png"/>
-           </a>
+      <a
+        className="hover:opacity-50 transition-duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          alt="facebook-link" 
+          src="/assets/facebook.png"
+          width={40}
+          height={40}
+        />
+      </a>
 
-           <a
-            className="hover:opacity-50 transition-duration-500"
-            href="https://www.facebook.com"
-            target="_blank"
-            rel="noreferrer"
-           >
-           <img alt="facebook-link" src="/assets/facebook.png"/>
-           </a>
+      <a
+        className="hover:opacity-50 transition-duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          alt="instagram-link" 
+          src="/assets/instagram.png"
+          width={40}
+          height={40}
+        />
+      </a>
 
-           <a
-            className="hover:opacity-50 transition-duration-500"
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noreferrer"
-           >
-           <img alt="instagram-link" src="/assets/instagram.png"/>
-           </a>
-
-           <a
-            className="hover:opacity-50 transition-duration-500"
-            href="https://github.com/Om-Khairnar"
-            target="_blank"
-            rel="noreferrer"
-           >
-           <img alt="Github-link" src="/assets/github.png" width="30" height="30"/>
-           </a>
-
-        </div>
-    )
-}
+      <a
+        className="hover:opacity-50 transition-duration-500"
+        href="https://github.com/Om-Khairnar"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          alt="Github-link"
+          src="/assets/github.png"
+          width={40}
+          height={40}
+        />
+      </a>
+    </div>
+  );
+};
 
 export default SocialMediaIcons;
