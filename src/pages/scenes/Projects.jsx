73f41c86b382694e0e1cd611e7728cad14474c3a
Om-khairnar <om.khairnar175@gmail.com>
2024-12-23 22:04:20 +0530

    remove some padding from the project overlay

--- src/pages/scenes/Projects.jsx
+++ src/pages/scenes/Projects.jsx
@@ -19,7 +19,7 @@ const projectVariant = {
 
 const Project = ({ title, overlayMessage, overlayLink }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-10 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
